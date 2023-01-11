8e9d136599f5d54da6d88b7ff6948f255b9e6068
AlloTheOne <Allo.busy@gmail.com>
2023-01-11 10:26:21 +0300

    Initial Commit

---
+++ WaterCal/WaterCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterCalApp.swift
+//  WaterCal
+//
+//  Created by Alaa Alabdullah on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WaterCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
